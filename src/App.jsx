5cb4e9b8d881a1d8b26c9d18f4fbda0046af8c66
Pratham <bhagatpratham101@gmail.com>
2023-06-12 01:13:28 +0530

    Message

--- src/App.jsx
+++ src/App.jsx
@@ -11,21 +11,45 @@ import Login from "./components/auth/Login";
 import ForgetPassword from "./components/auth/ForgetPassword";
 import ResetPassword from "./components/auth/ResetPassword";
 import LandingPage from "./components/LandingPage";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      50: "#f0e4ff",
+      100: "#cbb2ff",
+      200: "#a480ff",
+      300: "#7a4dff",
+      400: "#641bfe",
+      500: "#5a01e5",
+      600: "#5200b3",
+      700: "#430081",
+      800: "#2d004f",
+      900: "#14001f",
+    },
+  },
+  fonts: {
+    heading: `'Inter', sans-serif`,
+    body: `'Inter', sans-serif`,
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/forget-password" element={<ForgetPassword />} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </div>{" "}
-    </Router>
+    <ChakraProvider theme={theme}>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/forget-password" element={<ForgetPassword />} />
+            <Route path="/reset-password" element={<ResetPassword />} />
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>{" "}
+      </Router>
+    </ChakraProvider>
   );
 }
 
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -2,15 +2,88 @@
 import React from "react";
 import { useNavigate } from "react-router";
 import SocialSignIn from "./auth/SocialSignIn";
+import {
+  Button,
+  Center,
+  Container,
+  Heading,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  chakra,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  Flex,
+  IconButton,
+  Link,
+  LinkBox,
+  LinkOverlay,
+  Spacer,
+  Stack,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 const LandingPage = () => {
   const navigate = useNavigate();
   return (
     <div>
-      <h3>LandingPage</h3>
-      <button onClick={() => navigate("/login")}>Login</button>
-      <button onClick={() => navigate("/signup")}>Signup</button>
-      <SocialSignIn />
+      <Container maxW={"container.xl"}>
+        <Text
+          width={"100"}
+          textAlign={"start"}
+          ml={12}
+          fontSize="28.73px"
+          color={"blue.700"}
+        >
+          HyperBlog
+        </Text>
+        <Button m={4} colorScheme="brand" onClick={() => navigate("/login")}>
+          Login
+        </Button>
+        <Button m={4} colorScheme="brand" onClick={() => navigate("/signup")}>
+          Signup
+        </Button>
+        <SocialSignIn />
+      </Container>
+      <Container maxW="container.lg">
+        <Center p={2} minHeight="70vh">
+          <VStack>
+            <Container maxW="container.md" textAlign="center">
+              <Heading size="xl" mb={4} color="gray.700">
+                Ship Your Blogs 10X Faster With AI
+              </Heading>
+
+              <Text fontSize="xl" color="gray.500">
+                Be the first one to experience future of writing with our AI
+                powered Auto writer
+              </Text>
+
+              <Button
+                mt={8}
+                colorScheme="brand"
+                onClick={() => {
+                  window.open("", "_blank");
+                }}
+              >
+                I need this for $19/month →
+              </Button>
+
+              <Text my={2} fontSize="sm" color="gray.500">
+                108+ writers have signed up in the last 30 days
+              </Text>
+              <Image src="src/assets/Editor.jpeg" alt="editor" />
+            </Container>
+          </VStack>
+        </Center>
+      </Container>
     </div>
   );
 };
